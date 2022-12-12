da22be1ff1796e7db442f6eb7ffbef2d6ae7c335
Hayder1999 <hayderali1999@gmail.com>
2022-12-12 09:43:33 +0100

    refactor: naming made more consistent of the button component

--- my-cocktails/src/components/button/button.js
+++ my-cocktails/src/components/button/button.js
@@ -1,9 +1,9 @@
-import MUIButton from '@material-ui/core/Button';
+import MuiButton from '@material-ui/core/Button';
 
 const Button = (props) => {
     const { onClick, text, variant, color } = props;
     return (
-        <MUIButton variant={ variant } color={ color } onClick={ onClick }>{ text }</MUIButton>
+        <MuiButton variant={variant} color={color} onClick={onClick}>{text}</MuiButton>
     );
 };
 
